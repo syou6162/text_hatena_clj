f771f0b3d229fc22070139c3c873692af845dffe
Yasuhisa Yoshida <syou6162@gmail.com>
2012-09-16 15:16:54 +0900

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject text_hatena_clj "0.1.0-SNAPSHOT"
+  :description "Parser combinatorを使ったはてな記法のparser"
+  :url "https://github.com/syou6162/text_hatena_clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.jpoplett/fnparse "2.2.8"]]
+  :plugins [[lein-swank "1.4.4"]])
